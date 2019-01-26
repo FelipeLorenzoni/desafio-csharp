a4116dd9f1cd0eb59842fa5b8e2f9345f8fe8742
Felipe Lorenzoni <felipe_fl@hotmail.com>
2019-01-25 22:08:19 -0200

    Versionamento código incial desafio

---
+++ Algorithm.Logic.Tests/DroneUnitTest.cs
@@ -0,0 +1,133 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Logic.Tests
+{
+    [TestClass]
+    public class DroneUnitTest
+    {
+        [TestMethod]
+        public void Input_NNNNNLLLLL()
+        {
+            Assert.AreEqual("(5, 5)", Program.Evaluate("NNNNNLLLLL"));
+        }
+
+        [TestMethod]
+        public void Input_NLNLNLNLNL()
+        {
+            Assert.AreEqual("(5, 5)", Program.Evaluate("NLNLNLNLNL"));
+        }
+
+        [TestMethod]
+        public void Input_NNNNNXLLLLLX()
+        {
+            Assert.AreEqual("(4, 4)", Program.Evaluate("NNNNNXLLLLLX"));
+        }
+
+        [TestMethod]
+        public void Input_SSSSSOOOOO()
+        {
+            Assert.AreEqual("(-5, -5)", Program.Evaluate("SSSSSOOOOO"));
+        }
+
+        [TestMethod]
+        public void Input_S5O5()
+        {
+            Assert.AreEqual("(-5, -5)", Program.Evaluate("S5O5"));
+        }
+
+        [TestMethod]
+        public void Input_NNX2()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("NNX2"));
+        }
+        
+        [TestMethod]
+        public void Input_N123LSX()
+        {
+            Assert.AreEqual("(1, 123)", Program.Evaluate("N123LSX"));
+        }
+	
+		[TestMethod]
+        public void Input_NLS3X()
+        {
+            Assert.AreEqual("(1, 1)", Program.Evaluate("NLS3X"));
+        }
+
+		[TestMethod]
+        public void Input_NNNXLLLXX()
+        {
+            Assert.AreEqual("(1, 2)", Program.Evaluate("NNNXLLLXX"));
+        }
+
+        [TestMethod]
+        public void Input_N40L30S20O10NLSOXX()
+        {
+            Assert.AreEqual("(21, 21)", Program.Evaluate("N40L30S20O10NLSOXX"));
+        }
+
+        [TestMethod]
+        public void Input_NLSOXXN40L30S20O10()
+        {
+            Assert.AreEqual("(21, 21)", Program.Evaluate("NLSOXXN40L30S20O10"));
+        }
+
+        [TestMethod]
+        public void Input_NULL()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate(null)); // Entrada nula
+        }
+
+        [TestMethod]
+        public void Input_EMPTY()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("")); // Entrada vazia
+        }
+
+        [TestMethod]
+        public void Input_WHITESPACE()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("   ")); // Entrada espaço vazio
+        }
+
+        [TestMethod]
+        public void Input_123()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("123")); // Entrada inválida
+        }
+
+        [TestMethod]
+        public void Input_123N()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("123N")); // passos antes da direçao
+        }
+
+        [TestMethod]
+        public void Input_N2147483647N()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("N2147483647N")); // Overflow
+        }
+
+        [TestMethod]
+        public void Input_NNI()
+        {
+            Assert.AreEqual("(999, 999)", Program.Evaluate("NNI")); // Commando inválido
+        }
+
+        [TestMethod]
+        public void Input_N2147483647XN()
+        {
+            Assert.AreEqual("(0, 1)", Program.Evaluate("N2147483647XN")); // Overflow cancelado
+        }
+
+        [TestMethod]
+        public void Input_BIGSTRING()
+        {
+            Assert.AreEqual("(500, 500)", Program.Evaluate(new string(
+                Enumerable.Repeat('N', 1000).Concat(
+                Enumerable.Repeat('S', 500)).Concat(
+                Enumerable.Repeat('L', 1000)).Concat(
+                Enumerable.Repeat('O', 500)).ToArray())));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Algorithm.Logic
+{
+    using System;
+
+    public class Program
+    {
+        /// <summary>
+        /// PROBLEMA:
+        /// 
+        /// Implementar um algoritmo para o controle de posição de um drone emum plano cartesiano (X, Y).
+        /// 
+        /// O ponto inicial do drone é "(0, 0)" para cada execução do método Evaluate ao ser executado cada teste unitário.
+        /// 
+        /// A string de entrada pode conter os seguintes caracteres N, S, L, e O representando Norte, Sul, Leste e Oeste respectivamente.
+        /// Estes catacteres podem estar presentes aleatóriamente na string de entrada.
+        /// Uma string de entrada "NNNLLL" irá resultar em uma posição final "(3, 3)", assim como uma string "NLNLNL" irá resultar em "(3, 3)".
+        /// 
+        /// Caso o caracter X esteja presente, o mesmo irá cancelar a operação anterior. 
+        /// Caso houver mais de um caracter X consecutivo, o mesmo cancelará mais de uma ação na quantidade em que o X estiver presente.
+        /// Uma string de entrada "NNNXLLLXX" irá resultar em uma posição final "(1, 2)" pois a string poderia ser simplificada para "NNL".
+        /// 
+        /// Além disso, um número pode estar presente após o caracter da operação, representando o "passo" que a operação deve acumular.
+		/// Este número deve estar compreendido entre 1 e 2147483647.
+		/// Deve-se observar que a operação 'X' não suporta opção de "passo" e deve ser considerado inválido. Uma string de entrada "NNX2" deve ser considerada inválida.
+        /// Uma string de entrada "N123LSX" irá resultar em uma posição final "(1, 123)" pois a string pode ser simplificada para "N123L"
+        /// Uma string de entrada "NLS3X" irá resultar em uma posição final "(1, 1)" pois a string pode ser siplificada para "NL".
+        /// 
+        /// Caso a string de entrada seja inválida ou tenha algum outro problema, o resultado deve ser "(999, 999)".
+        /// 
+        /// OBSERVAÇÕES:
+        /// Realizar uma implementação com padrões de código para ambiente de "produção". 
+        /// Comentar o código explicando o que for relevânte para a solução do problema.
+        /// Adicionar testes unitários para alcançar uma cobertura de testes relevânte.
+        /// </summary>
+        /// <param name="input">String no padrão "N1N2S3S4L5L6O7O8X"</param>
+        /// <returns>String representando o ponto cartesiano após a execução dos comandos (X, Y)</returns>
+        public static string Evaluate(string input)
+        {
+            // TODO: Este método é o ponto de entrada para a lógica.
+            return "(X, Y)";
+        }
+    }
+}
